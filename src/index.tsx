3e7a8cde67043b229c21373037642f8c3801291a
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2020-10-23 14:44:32 +0700

    Added listIklan component adn iklanItem component

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import './index.css';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import { App } from './App';
-import * as serviceWorker from './serviceWorker';
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<React.StrictMode>
-  <App />
-</React.StrictMode>, document.getElementById('root'))
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
