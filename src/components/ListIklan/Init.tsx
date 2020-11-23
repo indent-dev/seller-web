abd7c226a1984e24a8245b6eee6508c7bb976c74
syaifuddinzuhri <mochammadsyaifuddinzuhri@gmail.com>
2020-11-23 21:28:59 +0700

    Change listIklan to dinamis list using useState()

---
+++ src/components/ListIklan/Init.tsx
@@ -0,0 +1,47 @@
+export const init = [
+  {
+    id: 1,
+    image: "https://placeimg.com/640/480/tech",
+    title: "Lenovo Legino 5",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae laboriosam harum ipsum. Deserunt quo expedita odio illum similique voluptatibus hic.",
+    date: "Sunday, 25 Oktober 2020",
+    price: 14500000,
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/640/480/tech",
+    title: "Lenovo Yoga Slim 7",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae laboriosam harum ipsum. Deserunt quo expedita odio illum similique voluptatibus hic.",
+    date: "Friday, 13 Oktober 2020",
+    price: 14999000,
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/640/480/tech",
+    title: "Lenovo Ideapad c340",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae laboriosam harum ipsum. Deserunt quo expedita odio illum similique voluptatibus hic.",
+    date: "Saturday, 9 September 2020",
+    price: 10999000,
+  },
+  {
+    id: 4,
+    image: "https://placeimg.com/640/480/tech",
+    title: "Lenovo Ideapad c340",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae laboriosam harum ipsum. Deserunt quo expedita odio illum similique voluptatibus hic.",
+    date: "Sunday, 9 September 2020",
+    price: 10999000,
+  },
+  {
+    id: 5,
+    image: "https://placeimg.com/640/480/tech",
+    title: "Lenovo Ideapad c340",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae laboriosam harum ipsum. Deserunt quo expedita odio illum similique voluptatibus hic.",
+    date: "Monday, 9 September 2020",
+    price: 10999000,
+  },
+];
--- src/components/ListIklan/ListIklan.tsx
+++ src/components/ListIklan/ListIklan.tsx
@@ -1,49 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { init } from "./Init";
 import IklanItem from "../IklanItem/IklanItem";
+import { message } from "antd";
+
+interface IItem {
+  id: number;
+  title: string;
+  image: string;
+  description: string;
+  price: number;
+  date: string;
+}
 
 const ListIklan = () => {
+  const [items, setItems] = useState<IItem[]>(init);
+
+  const deleteItem = (id: number) => {
+    const newItems = items.filter((item) => item.id !== id);
+    setItems(newItems);
+    message.info(`${id} deleted successfully!`);
+  };
+
   return (
     <div>
-      <IklanItem
-        id= {1}
-        image ={"https://placeimg.com/640/480/tech"}
-        title="Lenovo Legino 5"
-        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae laboriosam harum ipsum. Deserunt quo expedita odio illum similique voluptatibus hic."
-        date="Sunday, 25 Oktober 2020"
-        price= {14500000}
+      {items.map((item) => (
+        <IklanItem
+          key={item.id}
+          item={item}
+          deleteItem={() => deleteItem(item.id)}
         />
-      <IklanItem
-        id= {2}
-        image={"https://placeimg.com/640/480/tech"}
-        title="Lenovo Yoga Slim 7"
-        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae laboriosam harum ipsum. Deserunt quo expedita odio illum similique voluptatibus hic."
-        date="Friday, 13 Oktober 2020"
-        price= {14999000}
-        />
-      <IklanItem
-        id= {3}
-        image={"https://placeimg.com/640/480/tech"}
-        title="Lenovo Ideapad c340"
-        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae laboriosam harum ipsum. Deserunt quo expedita odio illum similique voluptatibus hic."
-        date="Saturday, 9 September 2020"
-        price={10999000}
-        />
-      <IklanItem
-        id= {4}
-        image={"https://placeimg.com/640/480/tech"}
-        title="Lenovo Ideapad c340"
-        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae laboriosam harum ipsum. Deserunt quo expedita odio illum similique voluptatibus hic."
-        date="Sunday, 9 September 2020"
-        price={10999000}
-        />
-      <IklanItem
-        id= {5}
-        image={"https://placeimg.com/640/480/tech"}
-        title="Lenovo Ideapad c340"
-        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae laboriosam harum ipsum. Deserunt quo expedita odio illum similique voluptatibus hic."
-        date="Monday, 9 September 2020"
-        price={10999000}
-      />
+      ))}
     </div>
   );
 };
