5a69ac160ebe131af21caaf73693bf2ca15abc16
Syaifuddin Zuhri <59104470+syaifuddinzuhri@users.noreply.github.com>
2020-11-28 20:45:13 +0700

    Change listIklan to dinamis list using useState() (#10)
    * Adding confirmation when delete iklan
    
    * feat(App): Adding search input and add button
    
    * Change listIklan to dinamis list using useState()
    
    Co-authored-by: Aditya Eka <aeka489@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 
-import { Layout, Input, Row, Avatar } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+
+import { Button, Layout, Input, Row, Avatar} from "antd";
+import { PlusOutlined, UserOutlined } from "@ant-design/icons";
+
 import ListIklan from "./components/ListIklan/ListIklan";
 import AddIklan from "./components/AddIklan/AddIklan";
 
 const flexStyle = {
   display: "flex",
   alignItems: "center",
-  justifyContent: "space-between",
-};
+  justifyContent: "space-between"
+}
+
 
 const { Search } = Input;
 const { Header, Footer, Content } = Layout;
@@ -18,10 +21,10 @@ function App() {
   return (
     <div>
       <Layout>
-        <Header style={flexStyle}>
-          <h1 style={{ margin: "0", color: "#fff", fontWeight: "bold" }}>
-            Seller Web
-          </h1>
+        <Header
+          style={flexStyle}
+        >
+          <h1 style={{ margin: "0", color: "#fff", fontWeight: "bold" }}>Seller Web</h1>
           <div style={flexStyle}>
             <h3 style={{ margin: "0 10px", color: "#fff" }}>Username</h3>
             <Avatar size="large" icon={<UserOutlined />} />
@@ -34,22 +37,19 @@ function App() {
             textAlign: "center",
           }}
         >
-          <Row
-            gutter={{ xs: 8, sm: 8, md: 24, lg: 24 }}
-            style={{
-              marginBottom: "50px",
-              padding: "0 50px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
+          <Row gutter={{ xs: 8, sm: 8, md: 24, lg: 24 }} style={{marginBottom: "50px", padding: "0 50px", display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between"}}>
+
             <Search
               placeholder="input search text"
               enterButton
               style={{ width: "400px" }}
             />
-            <AddIklan />
+            <Button type="primary" icon={<PlusOutlined />}>
+              Add Iklan
+            </Button>
+
           </Row>
 
           <ListIklan />
--- src/components/IklanItem/IklanItem.tsx
+++ src/components/IklanItem/IklanItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Row, Col, Divider, Button, Popconfirm, message } from "antd";
+import { Card, Row, Col, Divider, Button, Popconfirm } from "antd";
 import "./IklanItem.css";
 import {
   EditOutlined,
@@ -7,46 +7,43 @@ import {
   CalendarOutlined,
 } from "@ant-design/icons";
 
+interface IItem {
+  id: number;
+  title: string;
+  image: string;
+  description: string;
+  price: number;
+  date: string;
+}
+
 type props = {
-  id: number,
-  title: string,
-  image: string,
-  description: string,
-  price: number,
-  date: string
-}
+  item: IItem;
+  deleteItem: Function;
+};
 
-const text = 'Are you sure to delete this iklan?';
-
-const deleteIklan = (title: string) => {
-  message.info(`${title} deleted successfully!`);
-}
+const text = "Are you sure to delete this iklan?";
 
 const IklanItem = (props: props) => {
+  const item = props.item;
   return (
     <div>
       <Card className="styleCard">
         <Row gutter={{ xs: 8, sm: 8, md: 24, lg: 32 }}>
-          <Col xs={24} sm={24} md={3} lg={3}>
-            <img
-              style={{ width: "100%" }}
-              alt={props.description}
-              src={props.image}
-            />
+          <Col sm={24} md={3}>
+            <img style={{ width: "100%" }} alt={item.image} src={item.image} />
           </Col>
-          <Col xs={24} sm={24} md={21} lg={21} style={{ textAlign: "left" }}>
-            <Row gutter={{ xs: 8, sm: 8, md: 24, lg: 32 }}>
-              <Col xs={24} sm={24} md={6} lg={6}>
-                <h3>{props.title}</h3>
-                <h2>Rp. {props.price}</h2>
+          <Col sm={24} md={21} style={{ textAlign: "left" }}>
+            <Row gutter={{ sm: 8, md: 24 }}>
+              <Col sm={24} md={6}>
+                <h3>{item.title}</h3>
+                <h2>Rp. {item.price}</h2>
               </Col>
-              <Col xs={24} sm={24} md={11} lg={11}>
-                <p>{props.description}</p>
+              <Col sm={24} md={11}>
+                <p>{item.description}</p>
               </Col>
               <Col
-                xs={24}
                 sm={24}
-                lg={6}
+                md={6}
                 style={{
                   textAlign: "right",
                   margin: "10px",
@@ -60,7 +57,13 @@ const IklanItem = (props: props) => {
                 >
                   Edit
                 </Button>
-                <Popconfirm placement="top" title={text} onConfirm={() => deleteIklan(props.title)} okText="Yes" cancelText="No">
+                <Popconfirm
+                  placement="top"
+                  title={text}
+                  onConfirm={() => props.deleteItem(item.id)}
+                  okText="Yes"
+                  cancelText="No"
+                >
                   <Button
                     type="primary"
                     danger
@@ -68,7 +71,7 @@ const IklanItem = (props: props) => {
                     className="btn"
                     icon={<DeleteOutlined />}
                   >
-                  Delete
+                    Delete
                   </Button>
                 </Popconfirm>
               </Col>
@@ -77,7 +80,7 @@ const IklanItem = (props: props) => {
               <Divider style={{ margin: "10px 0" }} />
               <h5 style={{ color: "#999" }}>
                 <CalendarOutlined style={{ marginRight: "5px" }} />
-                {props.date}
+                {item.date}
               </h5>
             </Row>
           </Col>
